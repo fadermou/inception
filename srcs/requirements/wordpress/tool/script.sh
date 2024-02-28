9c012947e350dee8fce6102f0d32236ff5ed82d4
fadermou <fadermou@fadermou.42.fr>
2024-02-28 12:01:31 -0500

    push

--- srcs/requirements/wordpress/tool/script.sh
+++ srcs/requirements/wordpress/tool/script.sh
@@ -11,18 +11,21 @@ wp config create --dbname=$db_name \
     --dbpass=$db_pwd \
     --dbhost=mariadb \
     --allow-root
+
 wp core install --url=$DOMAIN_NAME \
                 --title=$WP_TITLE \
                 --admin_user=$WP_ADMIN_USR \
                 --admin_password=$WP_ADMIN_PWD \
                 --admin_email=$WP_ADMIN_EMAIL \
                 --allow-root
-# wp user create editor 
-wp user create editor --role=editor --user_pass=123 ####it's not working
+
+wp user create $WP_USR $WP_EMAIL --role=editor --user_pass=$WP_PWD --allow-root
+
 
 wp theme install astra --activate --allow-root
-# sed -i 's/\/run\/php\/php7.4-fpm.sock/9000/g' /etc/php/7.4/fpm/pool.d/www.conf
-# mkdir /run/php
+
 sed -i 's/^listen = .*/listen = wordpress:9000/' /etc/php/7.4/fpm/pool.d/www.conf
-mkdir /run/php #for the ip address
+
+mkdir /run/php #Unable to create the PID file (/run/php/php7.4-fpm.pid).: No such file or directory (2)
+
 php-fpm7.4 -F
